b01b0d9fa650b91a96254f31d82bb2907b815c73
marvinlaisney04 <laisneymarvin@gmail.com>
2022-03-09 15:26:16 +0100

    fix boostrap import and create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarteComponent } from './carte/carte.component';
 import { LocalisationComponent } from './localisation/localisation.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarteComponent,
-    LocalisationComponent
+    LocalisationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
